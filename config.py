56cf4de03a3905f0a5ad93870f27a47b47a28139
lpalmisa <luca.palmisano@ulb.be>
2020-11-12 08:31:53 +0100

    Module Projects - Basic Project Creation

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
+import os
 import dotenv
 
 # Load .env variables
 dotenv.load_dotenv()
 
-MODULES = []
+MODULES = [
+    "modules.projects"
+]
+
+GITHUB_ACCESS_TOKEN = os.getenv("GITHUB_ACCESS_TOKEN")
+PROJECTS_DIRECTORY = os.getenv("PROJECTS_DIRECTORY")
--- utils/parsers.py
+++ utils/parsers.py
@@ -1,10 +1,12 @@
 from classes.parser import ArgParser
 from classes.formatters import SubcommandHelpFormatter
 
-_main_parser = ArgParser(formatter_class=SubcommandHelpFormatter)
+_main_parser = ArgParser()
 
 def parse_args():
-    return _main_parser.parse_args()
+    args = _main_parser.parse_args()
+    if 'handler' in args:
+        args.handler(**args.__dict__)
 
 class SubParsers:
     commands = _main_parser.add_subparsers(title="Commands", description="Available commands", metavar="command", required=True)
