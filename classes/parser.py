59c700023217353cccef92df2380a0775601efb3
lpalmisa <luca.palmisano@ulb.be>
2020-11-12 08:11:31 +0100

    Fix - Formatter class for every sub-parsers

--- classes/parser.py
+++ classes/parser.py
@@ -1,7 +1,12 @@
 import sys
 from argparse import ArgumentParser
+from .formatters import SubcommandHelpFormatter
 
 class ArgParser(ArgumentParser):
+
+    def __init__(self, **kwargs):
+        kwargs['formatter_class'] = SubcommandHelpFormatter
+        super().__init__(**kwargs)
 
     def error(self, message):
         print(f"error: {message}")
